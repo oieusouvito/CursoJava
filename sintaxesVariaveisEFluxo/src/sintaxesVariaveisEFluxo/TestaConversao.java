09981b3307e95896e0e7ba4af94bae9c06edd425
Victor Araujo <dandf9@hotmail.com>
2023-01-24 22:11:44 -0300

    Aprendendo sobre o casting de variáveis.

---
+++ sintaxesVariaveisEFluxo/src/sintaxesVariaveisEFluxo/TestaConversao.java
@@ -0,0 +1,13 @@
+package sintaxesVariaveisEFluxo;
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		
+		System.out.println(valor);
+		
+		
+	}
+}
