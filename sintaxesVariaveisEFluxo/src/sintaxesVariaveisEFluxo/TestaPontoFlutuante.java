ab71350e1b86b057e5c5a96427880f4f49a55c16
Victor Araujo <dandf9@hotmail.com>
2023-01-24 22:07:16 -0300

    Adicionando novo teste com variável salário.

---
+++ sintaxesVariaveisEFluxo/src/sintaxesVariaveisEFluxo/TestaPontoFlutuante.java
@@ -0,0 +1,13 @@
+package sintaxesVariaveisEFluxo;
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario;
+		salario = 1250.70;
+		
+		System.out.println(salario + " Que loucura " + salario + " Testando ");
+		 
+	}
+}
