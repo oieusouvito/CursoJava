c36fbd5077da415b21be4a391d1caf2e735cbace
Victor Araujo <dandf9@hotmail.com>
2023-01-24 22:02:50 -0300

    Criando novo projeto para testar variáveis.

---
+++ sintaxesVariaveisEFluxo/src/sintaxesVariaveisEFluxo/TestaVariaveis.java
@@ -0,0 +1,13 @@
+package sintaxesVariaveisEFluxo;
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste.");
+
+		int idade;
+		idade = 20;
+		
+		System.out.println("Minha idade é: " + idade);
+	}
+}
