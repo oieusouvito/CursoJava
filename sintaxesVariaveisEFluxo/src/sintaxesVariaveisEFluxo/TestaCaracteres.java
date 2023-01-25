5c1d509a57d11de45f3f4d2b978a9ddfed07cd66
Victor Araujo <dandf9@hotmail.com>
2023-01-25 20:29:02 -0300

    Descobrindo sobre o tipo de variável String

---
+++ sintaxesVariaveisEFluxo/src/sintaxesVariaveisEFluxo/TestaCaracteres.java
@@ -0,0 +1,14 @@
+package sintaxesVariaveisEFluxo;
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		
+		String palavra = "alura cursos online de tecnologia";
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+}
+	
+}
