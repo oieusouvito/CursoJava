8c71edab4ac1b3ed558e45268585632c072d1923
Victor Araujo <dandf9@hotmail.com>
2023-01-26 09:08:25 -0300

    Criação de funcionalidade Cria conta

---
+++ bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,10 @@
+package bytebank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta primeriaConta = new Conta();
+		primeriaConta.saldo = 200;
+		System.out.println(primeriaConta.saldo);
+	}
+}
