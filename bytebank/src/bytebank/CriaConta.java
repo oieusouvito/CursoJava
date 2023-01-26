d0902eb22786d29997ad119221e95568a5cd36f7
Victor Araujo <dandf9@hotmail.com>
2023-01-26 09:21:23 -0300

    Criando uma segunda conta.

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -6,5 +6,15 @@ public class CriaConta {
 		Conta primeriaConta = new Conta();
 		primeriaConta.saldo = 200;
 		System.out.println(primeriaConta.saldo);
+		
+		primeriaConta.saldo += 100;
+		System.out.println(primeriaConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeira conta tem: " + primeriaConta.saldo);
+		System.out.println("Segunda conta tem: " + segundaConta.saldo);
 	}
 }
+
