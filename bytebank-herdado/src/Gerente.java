6137b27fb4ea7d9e391555ce5a9b92eb895596b7
Victor Araujo <dandf9@hotmail.com>
2023-01-28 16:29:49 -0300

    Utilizando composição

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,27 +1,27 @@
 
 public class Gerente extends Funcionario implements Autenticavel {
 	
-	private int senha;
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+	private AutenticacaoUtil autenticador;
+
+	public Gerente() {
+		this.autenticador = new AutenticacaoUtil();
 	}
 	
-
 	public double getBonificacao() {
 		 System.out.println("Chamando o método de bonificação do Gerente.");
 		 return super.getSalario();
 	}
 	
-	public int getSenha() {
-		return senha;
-	}
 	
+	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
 	}
+
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
+
+	}
+
 }
