cace57b36953728f9d1e9fc3551411fb7b652eaf
Victor Araujo <dandf9@hotmail.com>
2023-01-28 16:16:50 -0300

    Adicionando Interfaces

---
+++ bytebank-herdado/src/Autenticavel.java
@@ -0,0 +1,13 @@
+
+//contrato autenticavel
+	//quem assina esse contrato, precisa implementar
+		//metodo setsenha
+		//metodo autentica
+
+public abstract interface Autenticavel{
+
+	public abstract void setSenha(int senha);
+
+	public abstract boolean autentica(int senha);
+
+}
